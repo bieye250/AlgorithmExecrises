d9d8202ac86f8644da7d4ee9fc7bc3dbcb73c344
bieye250 <bieye250@gmail.com>
2022-02-13 22:26:32 +0800

    2162

---
+++ Leetcode-2162.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Test{
+    public static void main(String[] args) {
+
+    }
+
+    public int minCostSetTime(int startAt, int moveCost, int pushCost, int targetSeconds) {
+        
+        return 0;
+    }
+}
