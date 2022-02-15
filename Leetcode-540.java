96667181f9349b443969cee2493e8fd2f18d2e89
刘帅-奇瑞 <liushuai@139.com>
2022-02-15 22:21:23 +0800

    2165

--- Leetcode-540.java
+++ Leetcode-540.java
@@ -1,6 +1,6 @@
-public class Leetcode540 {
+class Leetcode {
     public static void main(String[] args) {
-        
+        System.out.println("hello");
     }
 
     public int singleNonDuplicate(int[] nums) {
--- Leetcode2165.java
+++ Leetcode2165.java
@@ -1,9 +1,36 @@
-public class Leetcode2165 {
+import java.util.Arrays;
+
+public class Leetcode2165{
     public static void main(String[] args) {
-        
+        Leetcode2165 l = new Leetcode2165();
+        l.smallestNumber(-7605);
     }
 
     public long smallestNumber(long num) {
-
+        if (num == 0)
+        return 0;
+    if (num > 0) {
+        char[] c = String.valueOf(num).toCharArray();
+        Arrays.sort(c);
+        int t = 0;
+        while (c[t] == '0')
+            t++;
+        if (c[0] == '0') {
+            c[0] = c[t];
+            c[t] = '0';
+        }
+        return Long.parseLong(String.valueOf(c));
+    } else {
+        char[] c = String.valueOf(-num).toCharArray();
+        Arrays.sort(c);
+        for(int i=0;i<(c.length%2==0?c.length/2:c.length/2+1);i++){
+            char t = c[i];
+            c[i] = c[c.length- 1 -i];
+            c[c.length-1-i]=t;
+        }
+        return -1*Long.parseLong(String.valueOf(c));
+    }//0567
+        }
+    
     }
 }
