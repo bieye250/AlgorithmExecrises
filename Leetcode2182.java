5745e276c1b49b21b439535295e5aa90fa665e39
bieye250 <bieye250@gmail.com>
2022-03-04 16:33:58 +0800

    2182

--- Leetcode2182.java
+++ Leetcode2182.java
@@ -1,30 +1,34 @@
 public class Leetcode2182 {
     public static void main(String[] args) {
-        
+        var l = new Leetcode2182();
+        l.repeatLimitedString("gapqzytcgvbqnyucmvhzusqrebydzqnlyqjlglssdhjgiecnritocbfexnvnmrgcoayorbmexhazxtwshari", 9);
     }
 
     public String repeatLimitedString(String s, int repeatLimit) {
         var a = new int[26];
-        s.chars().forEach(i->a[i-'a']++);
-        var flag = false;
+        s.chars().forEach(i -> a[i - 'a']++);
         var sb = new StringBuilder();
-        for(int i=25; i >= 0; i--){
-            if(a[i] != 0){
+        for (int i = 25; i >= 0; i--) {
+            if (a[i] != 0) {
                 int t = i - 1;
-                while(a[i] > repeatLimit){
-                    if(flag){
-                        while(t>=0 && a[t]==0) t--;
-                        if(t < 0) break;
-                        else{
-                            sb.append((char)a[t]);a[t]--;
-                        }
-                    }
-                    for(int j=0;i<repeatLimit;j++) sb.append((char)a[i]);
+                boolean flag = false;
+                while (a[i] > repeatLimit) {
+                    for (int j = 0; j < repeatLimit; j++) sb.append((char) (i + 'a'));
                     a[i] -= repeatLimit;
-                    flag = true;
+                        while (t >= 0 && a[t] == 0)
+                            t--;
+                        if (t < 0){
+                            flag = true;break;}
+                        else {
+                            sb.append((char)(t+'a'));
+                            a[t]--;
+                        }
                 }
-                if(a[i] > 0 && a[i] < repeatLimit)
-                    for(int j=0;i<repeatLimit;j++) sb.append((char)a[i]);
+                if (!flag && a[i] > 0 && a[i] <= repeatLimit) {
+                    int min = Math.min(repeatLimit, a[i]);
+                    for (int j = 0; j < min; j++) sb.append((char) (i + 'a'));
+                    a[i] = 0;
+                }
             }
         }
         return sb.toString();
