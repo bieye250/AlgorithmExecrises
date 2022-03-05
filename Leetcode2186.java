cf36be3cd0bd3ea80d01d1cc829bfde9d589fc7a
bieye250 <bieye250@gmail.com>
2022-03-05 14:59:14 +0800

    2186

--- Leetcode2186.java
+++ Leetcode2186.java
@@ -14,6 +14,20 @@ public class Leetcode2186 {
         for (int j = 0; j < 26; j++) {
             ans += Math.abs(a[j]-b[j]);
         }
-        return ans;
+        return ans; //执行时间：25ms
+        /**  执行时间：12ms
+        int[] a = new int[128];
+        int[] b = new int[128];
+        for(char c : s.toCharArray()){
+            a[c]++;
+        }
+        for(char c : t.toCharArray()){
+            b[c]++;
+        }
+        int ans = 0;
+        for(int i=0; i<128; i++){
+            ans += Math.abs(a[i]-b[i]);
+        }
+        return ans;*/
     }
 }
