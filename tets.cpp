2edae213ec717c7afebfb814f305cd86cab1e1be
bieye250 <bieye250@gmail.com>
2022-03-15 16:01:51 +0800

    2044, Ctrl+c

--- tets.cpp
+++ tets.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 int main(){
-    std::cout<<"hello world"<<std::endl;
-
-    std::vector<int> v{1,2,3};
-    int a = 8;
-    for(auto &p : v)
-        std::cout<<p;
+    
 }
