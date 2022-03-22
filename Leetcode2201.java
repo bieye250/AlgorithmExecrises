a25edcc37a4e8395d56d3aa77290c9b5eb9e0e6e
bieye250 <bieye250@gmail.com>
2022-03-22 15:08:48 +0800

    2201

--- Leetcode2201.java
+++ Leetcode2201.java
@@ -3,8 +3,7 @@ public class Leetcode2201 {
     public int digArtifacts(int n, int[][] artifacts, int[][] dig) {
         boolean[][] flag = new boolean[n][n];
         for(int i = 0; i < dig.length; i++) 
-            for(int j = 0; j < 2; j++)
-                flag[i][j] = true;
+            flag[dig[i][0]][dig[i][1]] = true;
         int ans = 0;
         for (int i = 0; i < artifacts.length; i++){
             int r1 = artifacts[i][0], l1 = artifacts[i][1];
