4fd7811e61588c90c41a422bcacdeba28eb8e113
bieye250 <bieye250@gmail.com>
2022-04-11 15:53:31 +0800

    357

---
+++ Leetcode357.java
@@ -0,0 +1,16 @@
+public class Leetcode357 {
+    public int countNumbersWithUniqueDigits(int n) {
+        int[] a = {1,10,91};
+        if (n <= 2) return a[n];
+        int ans = 91;
+        while (n > 2){
+            int t = 9;
+            for (int i=9;i>10-n;i--){
+                t *= i;
+            }
+            ans += t;
+            n--;
+        }
+        return ans;
+    }
+}
