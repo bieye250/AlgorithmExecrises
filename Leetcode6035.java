a5242bee61aedda6cc6fcc75f8b330eb1a03c6c0
bieye250 <bieye250@gmail.com>
2022-04-13 10:48:32 +0800

    6035

--- Leetcode6035.java
+++ Leetcode6035.java
@@ -1,23 +1,24 @@
 import java.util.*;
 public class Leetcode6035{
-    public List<List<Integer>> findWinners(int[][] matches) {
-        Map<Integer,Integer> loseMap = new HashMap();
-        Set<Integer> deque = new TreeSet();
-        Set<Integer> deque1 = new TreeSet();
-        for (int[] i : matches){ 
-            loseMap.put(i[1],loseMap.getOrDefault(i[1],0)+1);
-            deque.add(i[0]);
+    public long numberOfWays(String s) {
+        int[] f = new int[100010]; // 0
+        int[] v = new int[100010]; // 1
+        int len = s.length();
+        for (int i=0; i<len; i++){
+            if (s.charAt(i) == '0'){
+                f[i+1] = f[i] + 1;
+                v[i+1] = v[i]; 
+            }
+            else{
+                f[i+1] = f[i];
+                v[i+1] = v[i] + 1;
+            }
         }
-        for (int i : loseMap.keySet()){
-            if (deque.contains(i)) deque.remove(i);
-            if (loseMap.get(i) == 1) deque1.add(i);
+        long ans = 0L;
+        for (int i=0; i<len; i++){
+            if (s.charAt(i) == '0') ans += v[i]*(v[len]-v[i]);
+            else ans += f[i]*(f[len]-f[i]);
         }
-        List<List<Integer>> ans = new ArrayList();
-        List<Integer> l1 = new ArrayList();
-        List<Integer> l2 = new ArrayList();
-        for (int i : deque) l1.add(i);
-        for (int i : deque1) l2.add(i);
-        ans.add(l1);ans.add(l2);
         return ans;
     }
 }
