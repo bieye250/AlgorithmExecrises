5647247501d0b7f7023468e0b8ef0c4017340116
bieye250 <bieye250@gmail.com>
2022-04-19 10:35:17 +0800

    2244

---
+++ Leetcode2244.java
@@ -0,0 +1,16 @@
+public class Leetcode2244 {
+    public int minimumRounds(int[] tasks) {
+        Map<Integer,Integer> map = new HashMap();
+        for (int i : tasks) map.put(i,map.getOrDefault(i,0)+1);
+        int ans = 0;
+        for (int i : map.keySet()){
+            int t = map.get(i);
+            if (t < 2) return -1;
+            else{
+                if (t % 3 == 0) ans += t/3;
+                else ans += t/3+1;
+            }
+        }
+        return ans;
+    }
+}
