63f08d53c2ba7a4750b31e99f40c17ce3be866e3
bieye250 <beiye250@gmail.com>
2022-05-04 20:50:38 +0800

    1674C

---
+++ Codeforce1674C.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Codeforce1674C {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        while (q-- > 0){
+            String s = sc.next();
+            String t = sc.next();
+            if(t.equals("a")){System.out.print(1);continue;}
+            if(t.contains("a")) {System.out.println(-1);continue;}
+            System.out.println(1<<s.length());
+        }
+        sc.close();
+    }
+}
