fddb3a6eaa3a3e8f137f4b8c7690281d541b785c
bieye250 <beiye250@gmail.com>
2022-05-15 10:03:42 +0800

    6067

---
+++ Leetcode6067.java
@@ -0,0 +1,13 @@
+public class Leetcode6067 {
+    public int waysToSplitArray(int[] nums) {
+        long sum = 0L, pre = 0L;
+        for(int i=0; i<nums.length;i++) sum += nums[i];
+        int count = 0;
+        for(int i=0; i<nums.length-1;i++){
+            sum -= nums[i];
+            pre += nums[i];
+            if(pre >= sum) count++;
+        }
+        return count;
+    }
+}
