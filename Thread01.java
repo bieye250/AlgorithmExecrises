b0ebfa1b58f335cd82baeb1974f5d30d45797114
bieye250 <beiye250@gmail.com>
2022-05-20 17:03:19 +0800

    Run threads in order

--- Thread01.java
+++ Thread01.java
@@ -2,6 +2,8 @@ public class Thread01 {
     static int t = 1;
     static int cnt = 0;
     static Object lock = new Object();
+    static volatile int count = 0;
+    static volatile int index = 0;
     public static void main(String[] args) {
         Thread threadA = new Thread(()->{
             synchronized(lock){
@@ -56,8 +58,29 @@ public class Thread01 {
                 }
             }
         },"ThreadC");
-        threadA.start();
-        threadB.start();
-        threadC.start();
+        // threadA.start();
+        // threadB.start();
+        // threadC.start();
+
+        Thread threadD = new Thread(()->{
+            while(count < 5){
+                if(index == 0){
+                    System.out.println(Thread.currentThread().getName()+" "+index);
+                    count ++;
+                    index = 1;
+                }
+            }
+        },"threadD");
+        Thread threadE = new Thread(()->{
+            while(count < 5){
+                if(index == 1){
+                    System.out.println(Thread.currentThread().getName()+" "+index);
+                    count ++;
+                    index = 0;
+                }
+            }
+        },"threadE");
+        threadD.start();
+        threadE.start();
     }
 }
