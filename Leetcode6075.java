d60aa22d900c6a77eadb8446924d460c3d6874b8
bieye250 <beiye250@gmail.com>
2022-05-22 15:38:42 +0800

    6075

---
+++ Leetcode6075.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Leetcode6075 {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        int ans = 0, len = rocks.length, cur = 0;
+        for(int i=0; i<len; i++) rocks[i] = capacity[i]-rocks[i];
+        Arrays.sort(rocks);
+        while(additionalRocks > 0){
+            if(cur >= len) break;
+            if(rocks[cur] != 0){
+                additionalRocks -= rocks[cur];
+                if(additionalRocks >= 0) ans++;
+            }
+            else ans++;
+            cur++;
+        }
+        return ans;
+    }
+    
+}
