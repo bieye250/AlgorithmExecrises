4983cd3dd41f36c09e1abd63c04a6f1301d75dad
bieye250 <beiye250@gmail.com>
2022-06-05 11:27:53 +0800

    6091

---
+++ Leetcode6091.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Leetcode6091{
+    public int partitionArray(int[] nums, int k) {
+        int min = 1<<30, max = -1, ans = 0, len = nums.length;
+        Arrays.sort(nums);
+        min = nums[0]; max = nums[0];
+        for(int i=0; i<len; i++){
+            if(nums[i] > max) max = nums[i];
+            if(max - min > k){
+                ans++;
+                max = nums[i];
+                min = nums[i];
+            }
+        }
+        return ans+1;
+    }
+}
