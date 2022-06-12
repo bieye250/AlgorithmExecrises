e7d4057665c25023d6f73c0acd7b236c994f1beb
bieye250 <beiye250@gmail.com>
2022-06-12 12:18:44 +0800

    5070

---
+++ Leetcode5270.java
@@ -0,0 +1,22 @@
+public class Leetcode5270{
+    public int minPathCost(int[][] grid, int[][] moveCost) {
+        int row = grid.length, col = grid[0].length;
+        var dp = new int[col][2];
+        for(int i=0; i<col; i++) {dp[i][0] = grid[0][i];dp[i][1] = grid[0][i];}
+        for(int i=0; i<row-1; i++){
+            for(int j=0; j<col; j++){
+                int min = 1<<30;
+                for(int k=0; k<col; k++){
+                    int val = grid[i][k];
+                    min = Math.min(dp[k][1]+moveCost[val][j], min);
+                }
+                dp[j][0] = min + grid[i][j]; 
+                System.out.println(dp[j]+" ");
+            }
+            for(int k=0; k<col; k++)dp[k][1]=dp[k][0];
+        }
+        int ans = 1<<30;
+        for(int i=0; i<col; i++) ans = Math.min(dp[i][0],ans);
+        return ans;
+    }
+}
---
+++ Leetcode6097.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Leetcode6097 {
+    public boolean matchReplacement(String s, String sub, char[][] mappings) {
+        if(s.contains(sub)) return true;
+        var map = new HashMap<Character,ArrayList<Character>>();
+        int len = s.length(), mlen = mappings.length, slen = sub.length();
+        for(int i=0; i<mlen; i++){
+            if(!map.containsKey(mappings[i][0])){
+                var l = new ArrayList<Character>();
+                l.add(mappings[i][1]);
+                map.put(mappings[i][0],l);
+            }
+            else map.get(mappings[i][0]).add(mappings[i][1]);
+        }
+        
+    }
+}
