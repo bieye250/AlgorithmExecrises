50b9e19d98be1e215037a7cd750cc2795cfdcf9a
bieye250 <beiye250@gmail.com>
2022-06-21 12:16:50 +0800

    2302

---
+++ Leetcode2302.java
@@ -0,0 +1,12 @@
+public class Leetcode2302 {
+    public long countSubarrays(int[] nums, long k) {
+        long ans = 0L, sum = 0L;
+        for (int left = 0, right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum * (right - left + 1) >= k)
+                sum -= nums[left++];
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+}
