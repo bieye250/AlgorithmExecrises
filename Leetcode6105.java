749a69caa8311c919848ac79f2b3c1d45a4fc8a3
bieye250 <beiye250@gmail.com>
2022-06-25 23:19:56 +0800

    6105

---
+++ Leetcode6105.java
@@ -0,0 +1,15 @@
+public class Leetcode6105 {
+    public int maximumXOR(int[] nums) {
+        int ans = 0;
+        for(int i=30;i>=0;i--){
+            int cnt = 0;
+            for(int j=0;j<nums.length;j++){
+                if(((nums[j]>>i)&1) == 1) cnt++; 
+            }
+            if(cnt > 0){
+                ans += 1<<i;
+            }
+        }
+        return ans;
+    }
+}
