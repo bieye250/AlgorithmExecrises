774b7acbcb09d104587b8a7c74c4aa14fb1aeb4e
bieye250 <beiye250@gmail.com>
2022-07-03 11:36:21 +0800

    6111

---
+++ Leetcode6111.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Leetcode6111 {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        var ans = new int[m][n];
+        Arrays.fill(ans,-1);
+        int row=0, col=0;
+        boolean left = false, up = false;
+        while(head!=null){
+            while(col<n&&ans[row][col]<0&&head!=null){
+                ans[row][col] = head.val;
+                col++;
+                head = head.next;
+            }
+            col--;row++;
+            while(row<m&&ans[row][col]<0&&head!=null){
+                ans[row][col] = head.val;
+                row++;
+                head = head.next;
+            }
+            row--;col--;
+            while(col>=0&&ans[row][col]<0&&head!=null){
+                ans[row][col]= head.val;
+                col--;
+                head = head.next;
+            }
+            col++;row--;
+            while(row>=0&&ans[row][col]<0&&head!=null){
+                ans[row][col] = head.val;
+                row--;
+                head = head.next;
+            }
+            row++;col++;
+        }
+        return ans;
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
