c7d2fe4d803013208fea31bec198ccbbc5b60846
haishui <beiye250@gmail.com>
2022-09-10 16:00:56 +0800

    669

---
+++ Leetcode202209/Leetcode669.java
@@ -0,0 +1,14 @@
+package Leetcode202209;
+
+public class Leetcode669 {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root == null) return null;
+        if(root.val > high) return trimBST(root.left,low,high);
+        else if(root.val < low) return trimBST(root.right, low, high);
+        else {
+            root.left = trimBST(root.left,low,high);
+            root.right = trimBST(root.right,low,high);
+            return root;
+        }
+    }
+}
