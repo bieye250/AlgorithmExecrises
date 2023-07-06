ee79c301ecd961a5413990380f8d6685b4fe75ef
haishui <beiye250@gmail.com>
2023-07-06 16:25:47 +0800

    2178

---
+++ Leetcode2020307/Leetcode2178.java
@@ -0,0 +1,15 @@
+package Leetcode2020307;
+public class Leetcode2178 {
+    public List<Long> maximumEvenSplit(long finalSum) {
+        var ans = new ArrayList<Long>();
+        if(finalSum % 2 != 0) return ans;
+
+        for(long i=2; i<=finalSum; i+=2){
+            ans.add(i);
+            finalSum -= i;
+        }
+
+        ans.set(ans.size()-1, ans.get(ans.size()-1)+finalSum);
+        return ans;
+    }
+}
